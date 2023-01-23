1842b34c39d98f6ab30ec5a685c77404705b6dad
Diego Breskovit <81165451+dbreskovit@users.noreply.github.com>
2023-01-23 16:38:05 -0300

    🎉 Commit inicial

---
+++ src/main/java/com/unisoma/api/ApiApplication.java
@@ -0,0 +1,20 @@
+package com.unisoma.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String status(){
+		return "OK";
+	}
+}
---
+++ src/test/java/com/unisoma/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unisoma.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
